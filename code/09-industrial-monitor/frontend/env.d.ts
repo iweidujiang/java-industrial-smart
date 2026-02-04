2cac7dd8a6732fdb7246f86a46f9c62b0b75aa15
苏渡苇 <iyouzh@163.com>
2026-02-04 16:08:14 +0800

    Dockerfile

--- code/09-industrial-monitor/frontend/env.d.ts
+++ code/09-industrial-monitor/frontend/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly BASE_URL: string
+  // 可添加其他自定义 env 变量，如 VITE_API_URL
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
